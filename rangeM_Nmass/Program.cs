2cd81f0a755c10289da78ece9b7e4eb9c5c88231
semen.jons <semen.jons@yandex.ru>
2022-12-03 21:16:27 +0300

    сумма натуральных элементов в промежутке

---
+++ rangeM_Nmass/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите следующую число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+
+void TopNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    TopNumberSum(numberM, numberN, sum);
+}
+
+TopNumberSum(numberM, numberN, 0);
